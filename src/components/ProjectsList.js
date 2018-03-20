e3810ab02d6be8e75ec9db024d196dfe203491b1
luisf11 <luisfernandop1110@gmail.com>
2018-03-20 10:37:29 -0400

    Add bounce arrow in projects

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -2,8 +2,37 @@ import React from 'react';
 import Project from './Project';
 import {projects} from '../config/config';
 import {ContainerList,List} from '../shared/styled-components/projectList'
+import styled, { keyframes } from 'styled-components';
+
+const bounce = keyframes`
+    from {
+        transform: translateY(0);
+    }
+    to {
+      
+        transform: translateY(10px);
+       
+    }
+    }
+`;
+
+const ArrowContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
 
 
+
+const DownArrow = styled.div`
+    position: absolute;
+    width: 0; 
+    height: 0; 
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;  
+    border-top: 10px solid white;
+     animation: ${bounce} 1s linear infinite;
+`;
 
 const renderList = () =>{
     return(
@@ -24,6 +53,9 @@ const ProjectList = () => {
     return(
        <ContainerList>
            {renderList()}
+           <ArrowContainer>
+           <DownArrow/>
+           </ArrowContainer>
        </ContainerList>
     );
 
