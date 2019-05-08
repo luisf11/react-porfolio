ebd95d7c73b123acfe78904073692f623ae829a5
luisf11 <luisfernandop1110@gmail.com>
2019-05-08 11:08:19 -0400

    fix about text

--- src/modules/About.js
+++ src/modules/About.js
@@ -1,15 +1,23 @@
 import React from "react";
 import {about} from '../config/config'
-import {TextWrapper}from '../shared/styled-components/about';
+import styled from "styled-components";
 
 const Text = styled.p`
     color: white;
-    font-family: "Source Sans Pro";
-    font-size: 1em;
+    font-family: sans-serif;
+    font-size: 1.2em;
     /* margin: 0.75em 0 0.25em 0; */
-    margin: 1em;
-    opacity: 0.75;
+    margin: 5px;
+    opacity: 0.90;
 
+`;
+const TextWrapper = styled.div`
+    display:flex;
+    justify-content: flex-start;
+    margin: 10px;
+    height: 280px;
+    text-align: justify;
+    // background: #0c033852;
 `;
 
 const About = () => {
